2f80c0c3a7ddcc44da4b5bea5aa407c6942cbe37
OcimaOcram <marco.amico.pesci00@gmail.com>
2022-06-10 17:56:13 +0200

    esercizio completo

--- src/components/MyMusic.vue
+++ src/components/MyMusic.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="container">
-      <div>{{details.title}}</div>
-      <img :src="details.poster" alt="">
+  <div class="card">
+      
+    <img :src="details.poster" alt="">
+    <div>{{details.title}}</div>
+    <div>{{details.author}}</div>
+    <div>{{details.year}}</div>
+
+
+
   </div>
 </template>
 
@@ -9,14 +15,23 @@
 export default {
   name: 'MyMusic',
   props: {
-    details: Object
+    details: Array
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.container{
-    background-color: rgb(255, 255, 255);
+.card{
+    background-color: rgba(45, 90, 141, 0.452);
+    width: 150px;
+    min-height: 150px;
+    display: inline-block;
+    margin: 15px;
+    color: black;
+}
+img {
+    width: 75%;
+    height: 75%;
 }
 </style>
--- src/components/MyMusics.vue
+++ src/components/MyMusics.vue
@@ -38,6 +38,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .container{
-    background-color: rgb(255, 255, 255);
+    background-color: rgb(28, 40, 61);
 }
 </style>
